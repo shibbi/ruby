264d71f0cf06f5854831b03fe6c26f4c698a0c5f
shibbi <shibo.fang@gmail.com>
2015-03-06 14:09:35 -0800

    Implement phase II Searchable module

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -3,23 +3,21 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    cols, values = [], []
-    params.keys.each { |attr_name| cols << attr_name }
-    params.values.each { |value| values << value }
-
-    condition = (cols.map { |col| "#{col} = ?" }).join(' AND ')
-
-    DBConnection.execute2(<<-SQL, values)
+    cols, args = params.keys, params.values
+    where_line = (cols.map { |col| "#{col} = ?" }).join(' AND ')
+    data = DBConnection.execute(<<-SQL, *args)
       SELECT
         *
       FROM
         #{table_name}
       WHERE
-        #{condition}
+        #{where_line}
     SQL
+
+    parse_all(data)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
+  extend Searchable
 end
