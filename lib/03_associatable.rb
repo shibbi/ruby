c6d2d6fdae79f324b8898da8f2f3186a19b03e42
shibbi <shibo.fang@gmail.com>
2015-03-06 16:32:58 -0800

    Implement phase III Associatable and phase IVa assoc_options

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,41 +10,57 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{name}_id".to_sym
+    self.class_name = options[:class_name] || "#{name}".camelcase
+    self.primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    foreign_key_sym = "#{self_class_name.underscore}_id".to_sym
+    own_class_name = "#{name}".singularize.camelcase
+    self.foreign_key = options[:foreign_key] || foreign_key_sym
+    self.class_name = options[:class_name] || own_class_name
+    self.primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      foreign_key_value = send(options.foreign_key)
+      options.model_class.where(id: foreign_key_value).first
+    end
+    assoc_options[name] = options
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, to_s, options)
+    define_method(name) do
+      child = options.class_name.constantize
+      child.where(options.foreign_key => id)
+    end
   end
 
   def assoc_options
-    # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
